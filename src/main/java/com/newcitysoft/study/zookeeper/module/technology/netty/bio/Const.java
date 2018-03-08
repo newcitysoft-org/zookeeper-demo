5613e4e7233b1f11113788bd4490f48d5693e5ba
tianlixin <newcitysoft@163.com>
2018-03-08 15:25:33 +0800

    增加BIO实现的网络通信的demo。

---
+++ src/main/java/com/newcitysoft/study/zookeeper/module/technology/netty/bio/Const.java
@@ -0,0 +1,10 @@
+package com.newcitysoft.study.zookeeper.module.technology.netty.bio;
+
+/**
+ * @author lixin.tian@renren-inc.com
+ * @date 2018/3/8 13:53
+ */
+public class Const {
+
+    public static final int BIO_PORT = 5566;
+}
